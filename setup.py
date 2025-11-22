668d64bee94016bd280000caf674a9475155622e
Zhert <wxmusti66@gmail.com>
2025-11-22 15:21:29 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+# Installs this tool in your system
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "tornado",
+    version = "1.3.6",
+    author = "samogod",
+    license = "MIT",
+    keywords = ["Tornado", "Tor reverse shell", "reverse shell", "metasploit", "tor"],
+    url = "https://github.com/samogod/tornado",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Topic :: Security",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+    ],
+    install_requires=[
+        'colorama',
+        'requests',
+        'wget',
+        'stem',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tornado = tornado.__main__:main'
+        ]
+    },
+)
